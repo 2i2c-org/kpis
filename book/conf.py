ce2d4531b795889eb5f475d326e3e7be1ee8c05d
choldgraf <choldgraf@gmail.com>
2024-02-07 12:02:43 -0800

    Scriptify download command

--- book/conf.py
+++ book/conf.py
@@ -26,18 +26,6 @@ intersphinx_mapping = {
     "tc": ("https://compass.2i2c.org/en/latest/", None),
 }
 
-# Download key communities CSV file if it isn't there
-from pathlib import Path
-import urllib.request
-
-# URL of the file to download
-file_url = 'https://compass.2i2c.org/_downloads/8fa10f7f661246ec4fbe1515e254aa6d/key-communities.toml'
-file_local = Path("data/key-communities.toml")
-
-# Open the URL
-if not file_local.exists():
-    with urllib.request.urlopen(file_url) as response:
-        # Read the content of the response
-        file_content = response.read()
-        file_local.write_bytes(file_content)
-        print(f"Downloaded new key communities file to: {file_local}")
+# Ensure key communities is downloaded
+from subprocess import run
+run(["python", "download_key_communities.py"], cwd="scripts", check=True)
